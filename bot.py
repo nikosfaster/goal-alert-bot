7b0b47cf170543cd5a251a520cdf12009b1b06c9
nikosfaster <30376412+nikosfaster@users.noreply.github.com>
2026-01-18 10:39:35 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("BOT_TOKEN")
+print("DEBUG TOKEN =", TOKEN)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("🟢 Το bot είναι online!")
